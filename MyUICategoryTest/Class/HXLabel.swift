891bf919389080b1369ffdb80256e07df33cf025
SYkehaoran <18510666543@163.com>
2019-08-09 14:12:08 +0800

    完善带有边距的Label的计算

--- MyUICategoryTest/Class/HXLabel.swift
+++ MyUICategoryTest/Class/HXLabel.swift
@@ -3,21 +3,37 @@
 //  HXFundManager
 //
 //  Created by 柯浩然 on 2019/7/29.
-//  Copyright © 2019 China Asset Management Co., Ltd. All rights reserved.
 //
 
+/// 获取UIEdgeInsets在水平方向上的值
+func UIEdgeInsetsGetHorizontalValue(insets:UIEdgeInsets) -> CGFloat {
+    return insets.left + insets.right;
+}
+
+/// 获取UIEdgeInsets在垂直方向上的值
+func UIEdgeInsetsGetVerticalValue(insets:UIEdgeInsets)->CGFloat {
+    return insets.top + insets.bottom;
+}
 import UIKit
 
 class HXLabel: UILabel {
-
-   @objc var contentInset = UIEdgeInsets.zero
+    
+    @objc var contentInset = UIEdgeInsets.zero
     
     override var intrinsicContentSize: CGSize {
-    
-        var size = super.intrinsicContentSize
-        size.width = size.width + contentInset.left + contentInset.right
-        size.height = size.height + contentInset.top + contentInset.bottom
-        return size
+        
+        var preferredMaxLayoutWidth = self.preferredMaxLayoutWidth
+        if preferredMaxLayoutWidth <= 0 {
+            preferredMaxLayoutWidth = CGFloat.greatestFiniteMagnitude
+        }
+        
+        return sizeThatFits(CGSize(width: preferredMaxLayoutWidth, height: CGFloat.greatestFiniteMagnitude))
     }
     
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        var size = super.sizeThatFits(CGSize(width: size.width - UIEdgeInsetsGetHorizontalValue(insets: contentInset), height: size.height - UIEdgeInsetsGetVerticalValue(insets: contentInset)))
+        size.width += UIEdgeInsetsGetHorizontalValue(insets: self.contentInset)
+        size.height += UIEdgeInsetsGetVerticalValue(insets: self.contentInset)
+        return size;
+    }
 }
